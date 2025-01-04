5a3b6188ebdaeddef8a3903c4fd5f79574ac833d
Antonio Kolodji <antonio.kolodji@k-tech.com.hr>
2025-01-04 13:55:10 +0100

    brisanje komentara

--- email-templates/contact-form.php
+++ email-templates/contact-form.php
@@ -21,7 +21,6 @@ if( ! empty( $_POST['email'] ) ) {
 	$subject = 'k-Tech - Kontatk forma';
 
 	// Google reCaptcha secret Key
-	//$grecaptcha_secret_key = '6LcAGa0qAAAAANYXl_rRq3bQIqeUXORrCcApHgwJ';
 	$grecaptcha_secret_key = $_ENV['GRECAPTCHA_SECRET_KEY'];	
 
 	$from 	= $_POST['email'];
